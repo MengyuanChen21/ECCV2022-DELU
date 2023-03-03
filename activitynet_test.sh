ac81ee4db4cded2e2929c7001a81761b6b24d8a2
Mengyuan Chen <102446521+MengyuanChen21@users.noreply.github.com>
2023-03-03 12:57:08 +0800

    Create activitynet_test.sh

---
+++ activitynet_test.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+python test.py
+--dataset_name ActivityNet1.2 \
+--dataset AntSampleDataset \
+--num_class 100 \
+--path_dataset /path/to/CO2-ActivityNet-12 \
+--use_model DELU_ACT \
+--model_name ActivityNet \
+--max_seqlen 60 \
+--without_wandb
